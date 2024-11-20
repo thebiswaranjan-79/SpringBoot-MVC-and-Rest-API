ccdf52392ff595c9c3b4fa6b92151542dd5783d7
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-11-20 18:43:48 +0530

    Starting With the RestAPI, Created the Folder Structure

---
+++ springBootWebTutorial/src/main/java/com/codingShuttle/springBootWebTutorial/springBootWebTutorial/SpringBootWebTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.springBootWebTutorial.springBootWebTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebTutorialApplication.class, args);
+	}
+
+}
---
+++ springBootWebTutorial/src/test/java/com/codingShuttle/springBootWebTutorial/springBootWebTutorial/SpringBootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.springBootWebTutorial.springBootWebTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
